67d38855ef55600a4a3a27a75b7aad67b3d85fe3
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-18 19:46:36 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto;
+            string sobreNome;
+            string nome;
+
+            Console.Write("Digite o seu nome: ");
+            nome = Console.ReadLine();
+            
+            Console.Write("Digite o seu sobrenome: ");
+            sobreNome = Console.ReadLine();
+
+            nomeCompleto = $"{nome} {sobreNome}";
+            Console.WriteLine();
+            Console.WriteLine("Presione uma tecla para gerar seu nome de catálogo...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine($"Nome de catálogo: {sobreNome.ToUpper()} {nome}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NomeCompleto
 Resposta exercício "NomeCompleto"
+
+**Enunciado**
+
+Receba o nome e o sobrenome do usuário separadamente. Exiba o nome completo no formato padrão Nome Sobrenome e no formato de catálogo SOBRENOME, Nome.
+
+**Como executar**
+
+Abra o projeto e digite:
+
+dotnet run
